5ee79127794f46eebd90c32302dae5f6c9757918
benloos <ben.loos47@live.com>
2021-01-12 16:21:21 +0100

    m6 test stuff composer

--- emensa/vendor/composer/autoload_real.php
+++ emensa/vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit5137a0d339709badc657d3e1a0e1cf80
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5137a0d339709badc657d3e1a0e1cf80::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5137a0d339709badc657d3e1a0e1cf80($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5137a0d339709badc657d3e1a0e1cf80($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
